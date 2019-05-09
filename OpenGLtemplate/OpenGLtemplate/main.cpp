2bb41d24ec691b038cf68b2703bad2a96fcf8c07
Bigphess <xrp0308@gmail.com>
2019-05-09 13:24:26 +0900

    setup for openGL with glfw & glew

---
+++ OpenGLtemplate/OpenGLtemplate/main.cpp
@@ -0,0 +1,53 @@
+//
+//  main.cpp
+//  OpenGLtemplate
+//
+//  Created by 许若芃 on 2019/05/09.
+//  Copyright © 2019 许若芃. All rights reserved.
+//
+
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+void Render(void)
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+    {
+        glColor3f(1.0,0.0,0.0);
+        glVertex2f(0, .5);
+        glColor3f(0.0,1.0,0.0);
+        glVertex2f(-.5,-.5);
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex2f(.5, -.5);
+    }
+    glEnd();
+}
+
+int main(int argc, const char * argv[]) {
+    GLFWwindow* win;
+    if(!glfwInit()){
+        return -1;
+    }
+    win = glfwCreateWindow(640, 480, "OpenGL Base Project", NULL, NULL);
+    if(!win)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    if(!glewInit())
+    {
+        return -1;
+    }
+    glfwMakeContextCurrent(win);
+    while(!glfwWindowShouldClose(win)){
+        Render();
+        glfwSwapBuffers(win);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+    return 0;
+}
